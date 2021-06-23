df6e76150cab8c829b2a46bf4b14f89c7dfa5c48
trishantpahwa <trishantpahwa@gmail.com>
2021-06-23 13:56:23 +0530

    Added page logging for google analytics.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ import { UsersService } from './services';
 import './App.css';
 
 function App() {
+	function logPageRoute() {
+		// Removed for dev! Don't push to git
+		window.gtag('event', 'page_view', {
+			page_location: window.location.href,
+			page_path: window.location.pathname,
+			page_title: 'Wrec-er ' + window.location.pathname
+		});
+	}
 
 	useEffect(() => {
 		UsersService.checkSession();
