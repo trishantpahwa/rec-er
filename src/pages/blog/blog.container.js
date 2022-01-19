7d23e025fe9528f3cc5964e0b53a6fef494c1aa3
Trishant Pahwa <trishantpahwa@gmail.com>
2022-01-19 11:25:03 +0530

    Added feature to embed and view codepen and test code.

--- src/pages/blog/blog.container.js
+++ src/pages/blog/blog.container.js
@@ -10,13 +10,26 @@ function BlogContainer(props) {
 	const blogFiles = useSelector((state) => !!state.blogs && !!state.blogs.files && state.blogs.files) || null;
 	const [ data, setData ] = useState('');
 
+	const replaceCodePenTag = (md) => {
+		const codePenHashRegex = new RegExp("<codepen src=\"(.*)\"\ />", 'gm');
+		const codePenHash = md.match(codePenHashRegex);
+		let hash;
+		if(codePenHash && codePenHash.length) {
+			codePenHash.forEach((_codePenHash) => {
+				hash = _codePenHash.split('"')[1].split('/').pop();
+				md = md.replace(_codePenHash, `<iframe class="codepen-iframe" scrolling="no" src="https://codepen.io/trishantpahwa/embed/` + hash + `?default-tab=js%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">See the Pen <a href="https://codepen.io/trishantpahwa/pen/">` + hash + `Solution1</a> by Trishant Pahwa (<a href="https://codepen.io/trishantpahwa">@trishantpahwa</a>)on <a href="https://codepen.io">CodePen</a>.</iframe>`);
+			})
+		}
+		return md;
+	}
+
 	useEffect(() => {
 		dispatch(BlogsActions.getBlogFiles(props.match.params.id));
 	}, []);
 
 	useEffect(
 		() => {
-			if (blogFiles) setData(marked(blogFiles['data.md'].toString()));
+			if (blogFiles) setData(replaceCodePenTag(marked(blogFiles['data.md'].toString())));
 		},
 		[ blogFiles ]
 	);
