6cb557955ac989028399783184e1866ab8f6541a
Trishant Pahwa <trishantpahwa@gmail.com>
2022-03-26 22:43:26 +0530

    Changed aws region as env variable.

--- src/services/aws/aws.service.js
+++ src/services/aws/aws.service.js
@@ -3,7 +3,7 @@ import AWS from 'aws-sdk';
 AWS.config.update({
     accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY,
     secretAccessKey: process.env.REACT_APP_AWS_SECRET_KEY,
-    region: 'ap-south-1'
+    region: process.env.REACT_APP_AWS_REGION
 });
 
 export default AWS;
