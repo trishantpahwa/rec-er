25213b97a8acf9bcbfa7cfe25ce7c6863d7d6ba0
Trishant Pahwa <trishantpahwa@gmail.com>
2023-11-12 02:04:13 +0530

    Added performance analysis feature.

--- src/services/firebase/firebase.service.js
+++ src/services/firebase/firebase.service.js
@@ -1,14 +1,17 @@
 import { initializeApp } from "firebase/app";
+import { getPerformance } from "firebase/performance";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { firebaseConfig } from '../../store/firebase.config';
 
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 const auth = getAuth();
 const googleProvider = new GoogleAuthProvider();
 
 const db = getFirestore();
+
+getPerformance(app);
 
 
 export { auth, db, googleProvider, signInWithPopup };
