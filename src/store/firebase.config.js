15e9f81ed563dc28875f3b77908cee2c440133fe
Trishant Pahwa <trishantpahwa@gmail.com>
2026-01-30 23:50:01 +0530

    Refactor configuration files to enhance security and organization by using environment variables for Firebase credentials, update metadata structure, and improve .gitignore entries.

--- src/store/firebase.config.js
+++ src/store/firebase.config.js
@@ -1,10 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-	apiKey: "AIzaSyD9e75PbvZDNEPaZcwyY02YuKqmYGJcWI8",
-	authDomain: "rec-er-trishant.firebaseapp.com",
-	projectId: "rec-er-trishant",
-	storageBucket: "rec-er-trishant.firebasestorage.app",
-	messagingSenderId: "99083705518",
-	appId: "1:99083705518:web:ec8eac126d9b10830da13f",
-	measurementId: "G-MP439N6RFC"
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
