2f677745c25a19c2583909f16e0ed4981d55d4ba
Trishant Pahwa <trishantpahwa@gmail.com>
2026-02-01 23:25:09 +0530

    Remove Firebase configuration example and update AWS service to use NEXT_PUBLIC environment variables

--- src/services/aws/aws.service.js
+++ src/services/aws/aws.service.js
@@ -1,9 +1,9 @@
 import AWS from 'aws-sdk';
 
 AWS.config.update({
-    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY,
-    secretAccessKey: process.env.REACT_APP_AWS_SECRET_KEY,
-    region: process.env.REACT_APP_AWS_REGION
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_KEY,
+    region: process.env.NEXT_PUBLIC_AWS_REGION
 });
 
 export default AWS;
